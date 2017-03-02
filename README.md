f389e5270966b736c13e075f3305593682a319d1
Jennifer Oakes <jenniferoakes@Jennifers-MacBook-Pro.local>
2017-03-02 16:30:41 -0500

    Added traffic data files

--- README.md
+++
@@ -1,4 +0,0 @@
-This project will analyze DC government data on traffic citations in various 2016 months.
-
-One object of this project will be to make the code extensible so that further data analysis can occur.
-The program will be written in Node.js and will use separate modules for the various pieces of the solution.
--- src/parse.js
+++ src/parse.js
@@ -1,26 +1,27 @@
 
-let filessystem = require('fs');
 //*created a variable //this points to 'file system' inside 'node'
-
-
-//*made it possible to export the results of the function to the require line in the other file that calls it
-//*used readFile function to pull in moving jan 2016 data
-//*put a function inside of the readFile function to do something (print out contents of the csv january file)
-modules.export = filessystem.readFile('./moving_jan_2016.csv',
-  function printOutTheContents(possibleError, contentsOfTheFile) {
-
-    let rows = stringValue.split('\n'); //splitfind newlines, find where someone hit enter
-    console.log(rows); //puts the rows in an array
-
-    let stringValue = contentsOfTheFile.toString(); //all objects have a function called toString, converts contents into a string
-    console.log(stringValue); //buffer allows us to store data in a compressed container // produces one big string
-    //what if I wanted to get different pieces of the string
-    //pressing enter is an invisible character, so we can split on that invisible character
-
-    return; //this will be usuable data
-});
-
-
 
 //I need to parse the lines of text in the files and separating them into an array of arrays:
 //one array for each line, and each entry being an array of the values for that line
+
+module.exports = function parse(fileName) {
+  let filesystem = require('fs');
+
+  let inputData = filesystem.readFileSync(fileName);
+  let stringData = inputData.toString();
+  let entriesData = stringData.split('\n');
+//we made an array of strings that are each one row of the fileName
+//trying to step over the array, looking at each row
+//with each row we want to split at the commas
+  let returnData = [];
+
+  for (let index = 0; index < entriesData.length ; index++ ) {
+    returnData.push(entriesData[index].split(','));
+  }
+
+  console.log(returnData);
+  return returnData;
+};
+
+
+// parse('../simple_data/moving_jan_2016.csv');
