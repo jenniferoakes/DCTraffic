114a0ab674c9686ad7ff13f75a1f0646a5ee8a27
Jennifer Oakes <jenniferoakes@Jennifers-MacBook-Pro.local>
2017-03-03 22:04:26 -0500

    Solved other parking ticket questions by producing a keys object that called a function to let me see the number of license plate tickets

--- src/parking.js
+++ src/parking.js
@@ -1,6 +1,11 @@
 
 let dataParcer = require('./parse.js'); //if there is a problem with data, put outside in function
+
+// if/then
+
+// TODO create function that wraps all of this
 let parkingData = dataParcer('./simple_data/parking_feb_2016.csv'); //this calls data function to return array of arrays
+
 
 
 
@@ -10,6 +15,7 @@ let violationCodeIndex = parkingData[0].indexOf('VIOLATION_CODE');
 let plateIndex = parkingData[0].indexOf('RP_PLATE_STATE');
 
 let frequency = {};
+let frequecyLicensePlate = {};
 
 
 //change to module.exports later
@@ -24,6 +30,15 @@ for (let index = 1; index < parkingData.length -1 ; index++) { //go back and add
       frequency[parkingData[index][violationCodeIndex]]++;
     }
   }
+
+  if (parkingData[index][plateIndex] !== '') {
+    if (!frequecyLicensePlate[parkingData[index][plateIndex]]) { //have I seen this code?
+      frequecyLicensePlate[parkingData[index][plateIndex]] = 1; //'no' this  the first time, so i will set it to one -----is saying that when I see T120, it sounds as one, and goes into the object
+    } else {
+      frequecyLicensePlate[parkingData[index][plateIndex]]++;
+    }
+  }
+
 }
 
 // this figures out which code has the highest frequency
@@ -41,6 +56,18 @@ myKeys.forEach(function findHighestValue(tickettype) {
   }
 });
 
+let myLicenseKeys = Object.keys(frequecyLicensePlate);
+let maxLicensePlaceValue = 0;
+let maxLicensePlate;
+
+myLicenseKeys.forEach(function highestState(tickettype) {
+  // console.log( frequency[key] );
+  if (frequecyLicensePlate[tickettype] > maxLicensePlaceValue) {
+    maxLicensePlaceValue = frequecyLicensePlate[tickettype];
+    maxLicensePlate = tickettype;
+  }
+});
+
 let numberOfParkingTickets = Object.keys(frequency).length;
 
 
@@ -53,7 +80,9 @@ console.log('Most common violation type: ', maxTicketType);
 console.log('Number of different types of parking tickets: ', numberOfParkingTickets);
 
 // What state license plate gets the most tickets?
-
+console.log('frequency license plate',frequecyLicensePlate);
+console.log('State with highest tickets', maxLicensePlate); //state license plate that has the most tickets
+console.log('# of tickets for state with highest amount', maxLicensePlaceValue); //number of tickets for state license plate that has the most tickets
 
 // X, Y, OBJECTID, ROWID_, DAY_OF_WEEK, HOLIDAY, WEEK_OF_YEAR, MONTH_OF_YEAR, ISSUE_TIME,
 // VIOLATION_CODE, VIOLATION_DESCRIPTION, LOCATION, RP_PLATE_STATE, BODY_STYLE,
